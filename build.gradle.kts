b5ef70964575f6d596ad8a0db4854ee979a1d69e
MxShun <16466322+MxShun@users.noreply.github.com>
2021-08-08 12:25:36 +0900

    detektを入れる

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,43 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     idea
     kotlin("jvm") version "1.4.21" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.17.1"
 }
 
-allprojects {
-    repositories {
-        mavenCentral()
+repositories {
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven") }
+}
+
+detekt {
+    toolVersion = "1.17.1"
+    buildUponDefaultConfig = true
+    config = files("detekt/detekt.yml")
+
+    val sources = subprojects.flatMap {
+        listOf(
+            "${it.projectDir}/src/main/kotlin",
+            "${it.projectDir}/src/test/kotlin"
+        )
     }
+    input = files(sources)
+}
+
+project.dependencies {
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.17.1")
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt> {
+    jvmTarget = "11"
 }
 
 subprojects {
     apply {
         plugin("org.jetbrains.kotlin.jvm")
+    }
+
+    repositories {
+        mavenCentral()
     }
 
     dependencies {
