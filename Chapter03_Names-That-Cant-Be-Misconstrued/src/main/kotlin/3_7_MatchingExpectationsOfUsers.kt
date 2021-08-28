6cf915f249bc2280f67f6d1c9f29030d4f4f9e82
MxShun <16466322+MxShun@users.noreply.github.com>
2021-08-28 13:07:02 +0900

    abstractで抽象化

--- Chapter03_Names-That-Cant-Be-Misconstrued/src/main/kotlin/3_7_MatchingExpectationsOfUsers.kt
+++ Chapter03_Names-That-Cant-Be-Misconstrued/src/main/kotlin/3_7_MatchingExpectationsOfUsers.kt
@@ -1,16 +1,10 @@
 @file:Suppress("MatchingDeclarationName")
-class StatisticsCollector {
-    fun addSample(x: Double) {}
+abstract class StatisticsCollector {
+    abstract fun addSample(x: Double)
 
     // ✕コストが高いとは思わずに呼び出してしまう
-    fun getMean(): Double {
-        // 過去すべてのデータをイテレートして、その場で平均値を計算する
-        return 0.0
-    }
+    abstract fun getMean(): Double
 
     // 〇コストが高いことを見越した実装ができる
-    fun computeMean(): Double {
-        // 過去すべてのデータをイテレートして、その場で平均値を計算する
-        return 0.0
-    }
+    abstract fun computeMean(): Double
 }
